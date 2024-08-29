eda56b86f17910c20c85543f3ce995a49c48436c
VatsalKotha <vatsalkotha@gmail.com>
2024-08-29 13:28:10 +0530

    ADDED home

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import OTP from "./pages/OTP"
+import Home from "./pages/Home"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/otp" element={<OTP />} />
+        <Route path='/home' element={<Home/>}></Route>
       </Routes>
     </Router>
   );
