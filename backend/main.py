be7fac6194c33fca623a8884bfb65376df628be7
Meet Chavan <meetchavan24@gmail.com>
2024-08-29 14:44:12 +0530

    added api for adding product at fps side

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from settings.security import init_jwt
 from routes.auth_routes import auth_bp
 from routes.main_routes import main_bp
+from routes.fps_routes import fps_bp
 
 app = Flask(__name__)
 init_jwt(app)
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(fps_bp, url_prefix="/fps")
 app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
--- backend/models/globalproduct_model.py
+++ backend/models/globalproduct_model.py
@@ -1,14 +1,18 @@
 class GlobalProduct:
-    def __init__(self, product_id, product_name, product_image):
+    def __init__(self, product_id, product_name, product_image, product_size, product_category):
         self.product_id = product_id
         self.product_name = product_name
         self.product_image = product_image  
+        self.product_size = product_size
+        self.product_category = product_category
 
     def to_dict(self):
         return {
             "product_id": self.product_id,
             "product_name": self.product_name,
             "product_image": self.product_image,
+            "product_size": self.product_size,
+            "product_category": self.product_category,
         }
 
     @classmethod
@@ -17,4 +21,6 @@ class GlobalProduct:
             product_id=data.get("product_id"),
             product_name=data.get("product_name"),
             product_image=data.get("product_image"),
+            product_size=data.get("product_size"),
+            product_category=data.get("product_category"),
         )
